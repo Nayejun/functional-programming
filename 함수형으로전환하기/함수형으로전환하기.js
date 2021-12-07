8ccb834fb0e31555fccadfb8c86b33330c99a83b
4YJ <ohman0705@naver.com>
2021-12-07 20:05:52 +0900

    함수형으로전환하기

---
+++ 함수형으로전환하기/함수형으로전환하기.js
@@ -0,0 +1,39 @@
+let users = [
+  {id: 1, name: 'ID', age: 36},
+  {id: 2, name: 'BJ', age: 63},
+  {id: 3, name: 'JM', age: 45},
+  {id: 4, name: 'PJ', age: 13},
+  {id: 5, name: 'HA', age: 26},
+  {id: 6, name: 'JE', age: 64},
+  {id: 7, name: 'JI', age: 12},
+  {id: 8, name: 'MP', age: 16}
+];
+// 1. 명령형 코드
+//     1) 30세 이상인 users를 거른다
+let temp_users = [];
+for (let i = 0; i < users.length; i++) {
+  if (users[i].age >= 30) {
+    temp_users.push(users[i]);
+  }
+}
+console.log(temp_users)
+//     2) 30세 이상인 users의 names를 수집한다
+let names = [];
+for (let i = 0; i < temp_users.length; i++) {
+  names.push(temp_users[i].name);
+}
+console.log(names);
+//     3) 30세 미만인 users를 거른다
+let temp_userss = [];
+for (let i = 0; i < users.length; i++) {
+  if (users[i].age >= 30) {
+    temp_userss.push(users[i]);
+  }
+}
+console.log(temp_userss)
+//     4) 30세 미만인 users의 ages를 수집한다.
+let ages = [];
+for (let i = 0; i < temp_users.length; i++) {
+  ages.push(temp_users[i].age);
+}
+console.log(ages);
