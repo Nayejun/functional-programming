2efa7a9deeb5ddc47eb0d11e28504f31c1011cb2
4YJ <ohman0705@naver.com>
2021-12-08 16:54:00 +0900

    map 추가

--- 함수형으로전환하기/함수형으로전환하기.js
+++ 함수형으로전환하기/함수형으로전환하기.js
@@ -1,72 +1,114 @@
 let users = [
   {id: 1, name: 'ID', age: 36},
-  {id: 2, name: 'BJ', age: 63},
-  {id: 3, name: 'JM', age: 45},
-  {id: 4, name: 'PJ', age: 13},
-  {id: 5, name: 'HA', age: 26},
-  {id: 6, name: 'JE', age: 64},
-  {id: 7, name: 'JI', age: 12},
-  {id: 8, name: 'MP', age: 16}
+  {id: 2, name: 'BJ', age: 32},
+  {id: 3, name: 'JM', age: 32},
+  {id: 4, name: 'PJ', age: 27},
+  {id: 5, name: 'HA', age: 25},
+  {id: 6, name: 'JE', age: 26},
+  {id: 7, name: 'JI', age: 31},
+  {id: 8, name: 'MP', age: 23}
 ];
-// 1. 명령형 코드
-//     1) 30세 이상인 users를 거른다
+
+//30세 이상
 let temp_users = [];
 for (let i = 0; i < users.length; i++) {
   if (users[i].age >= 30) {
     temp_users.push(users[i]);
   }
 }
-console.log(temp_users)
-//     2) 30세 이상인 users의 names를 수집한다
+console.log(temp_users);
+
+//30세 이상 이름
 let names = [];
 for (let i = 0; i < temp_users.length; i++) {
   names.push(temp_users[i].name);
 }
 console.log(names);
-//     3) 30세 미만인 users를 거른다
-let temp_userss = [];
+
+//30세 미만
+let temp_users = [];
 for (let i = 0; i < users.length; i++) {
-  if (users[i].age >= 30) {
-    temp_userss.push(users[i]);
+  if (users[i].age < 30) {
+    temp_users.push(users[i]);
   }
 }
-console.log(temp_userss)
-//     4) 30세 미만인 users의 ages를 수집한다.
+console.log(temp_users);
+
+//30세 미만 나이
 let ages = [];
 for (let i = 0; i < temp_users.length; i++) {
   ages.push(temp_users[i].age);
 }
 console.log(ages);
 
-// 2. _filter, _map으로 리팩토링
-function _filter(users, predi) {
+function _filter(list, predi) {
   let new_list = [];
-  for (let i = 0; i < users.length; i++) {
-    if (predi(users[i])) {
-      new_list.push(users[i]);
+  for (let i = 0; i < list.length; i++) {
+    if (predi(list[i])) {
+      new_list.push(list[i]);
     }
-    return new_list;
   }
+  return new_list;
+}
+
+function _map(list, mapper) {
+  let new_list = [];
+  for (let i = 0; i < list.length; i++) {
+    new_list.push(mapper(list[i]));
+  }
+  return new_list;
 }
 
 console.log(
     _filter(users, function (user) {
       return user.age >= 30;
-    })
-);
+    }));
+
 console.log(
     _filter(users, function (user) {
       return user.age < 30;
-    })
-);
+    }));
+
+let over_30 = _filter(users, function (user) {
+  return user.age >= 30;
+});
 console.log(
-    _filter([1, 2, 3, 4], function (num) {
-      return num % 2;
-    })
-);
+    _map(over_30, function (user) {
+      return user.name;
+    }));
+
+let under_30 = _filter(users, function (user) {
+  return user.age < 30;
+});
+console.log(
+    _map(under_30, function (user) {
+      return user.age;
+    }));
+/*
+    console.log(
+        _filter([1, 2, 3, 4], function(num) { return num % 2; }));
+    console.log(
+        _filter([1, 2, 3, 4], function(num) { return !(num % 2); }));
+*/
+console.log(
+    _map(
+        _filter(users, function (user) {
+          return user.age >= 30;
+        }),
+        function (user) {
+          return user.name;
+        }));
 
 console.log(
-    _filter([1, 2, 3, 4], function (num) {
-      return !(num % 2);
-    })
-);
+    _map(
+        _filter(users, function (user) {
+          return user.age < 30;
+        }),
+        function (user) {
+          return user.age;
+        }));
+
+console.log(
+    _map(
+        _filter(users, user => user.age < 30),
+        user => user.name));
