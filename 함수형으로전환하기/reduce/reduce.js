82b620eabdb8e1f40c31a266eb3a7e1dd36b5b13
4YJ <ohman0705@naver.com>
2021-12-13 20:38:38 +0900

    reduce

---
+++ 함수형으로전환하기/reduce/reduce.js
@@ -0,0 +1,25 @@
+// 로직을 축약시킬 수 있다
+let slice = Array.prototype.slice;
+function _rest(list, num) {
+  return slice.call(list, num || 1);
+}
+function _reduce(list, iter, memo) {
+  if (arguments.length == 2) {
+    memo = list[0];
+    list = _rest(list);
+  }
+  _each(list, function (val) {
+    memo = iter(memo, val);
+  });
+  return memo;
+}
+
+console.log(
+    _reduce([1, 2, 3], add, 10)
+);// 16
+
+// 세번째 인자를 생략하고 하는 법
+console.log(
+    _reduce([1, 2, 3], add)
+);
+// 6
